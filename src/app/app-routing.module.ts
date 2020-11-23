3640c892821088fb26825195ef0b78fada2c36a0
bawwa.16@cse.mrt.ac.lk <bawwa.16@cse.mrt.ac.lk>
2020-11-23 08:02:26 +0000

    File generated by Skaffolder - user: bawwa.16@cse.mrt.ac.lk

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,32 @@
+// DEPENDENCIES
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+// SECURITY
+import { AuthGuard } from './security/auth.guard';
+
+/**
+ * WEB APP ROUTES
+ */
+const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+
+    /* START MY VIEWS */
+
+    { path: 'users', loadChildren: './pages/user-list/user-list.module#UserListModule', canActivate: [AuthGuard] },
+
+ /* END MY VIEWS */
+
+    // SECURITY
+    { path: 'login', loadChildren: './pages/login/login.module#LoginModule' }
+];
+
+/**
+ * ROUTING MODULE
+ */
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
